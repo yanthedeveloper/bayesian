89086b88712ab98686176d747c16070657b038aa
yalinyoh <yalinyoh@gmail.com>
2020-04-19 16:50:11 -0400

    Adding the python file that will generate my data

---
+++ percentages.py
@@ -0,0 +1,7 @@
+# I need to generate 500 lines of data
+# Each line of data will represent a patient. Each value will represent if they are 
+#     experiencing a specific symptom or not. There will also be numerical values
+#     for the categories that require we know how many of something they have i.e. 
+#     (parents with disease)
+# I am going to need to build a loop that will go through and generate either a value for each
+#     of the symptoms.
